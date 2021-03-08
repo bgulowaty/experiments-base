980f8528fd79bed442cb95012c284b519cbb4ecd
Bogdan Gulowaty <b.gulowaty@gmail.com>
2021-03-08 20:12:37 +0100

    Simplify

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+set -xe
+
 BASE_DIR=$(dirname "$(readlink -f "$0")")
+
+docker build -f ${BASE_DIR}/docker/Dockerfile -t bg/jupyter-experiment-env ${BASE_DIR}/docker/
+
 PARENT_DIR_OF_BASE_DIR=$(dirname "$BASE_DIR")
 export JUPYTER_DIR=${JUPYTER_DIR:-$PARENT_DIR_OF_BASE_DIR}
 
